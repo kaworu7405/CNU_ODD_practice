11cdf2cb23fe5e410c7aacff093a12a100dd010e
kaworu7405 <73168867+kaworu7405@users.noreply.github.com>
2021-02-09 10:35:16 +0900

    Update cyclictest_scripts.sh

--- cyclictest_scripts.sh
+++ cyclictest_scripts.sh
@@ -1,23 +1,5 @@
 #!/bin/bash
 
-#stress test할 test에 y로 설정, test하지않을 거라면 n로 설정
-no_background='y'
-list[0]=${no_background}
-hackbench='y'
-list[1]=${hackbench}
-iperf='y'
-list[2]=${iperf}
-cpu_stress='y'
-list[3]=${cpu_stress}
-memory_stress='y'
-list[4]=${memory_stress}
-hdd_stress='y'
-list[5]=${hdd_stress}
-virtual_memory_stress='y'
-list[6]=${virtual_memory_stress}
-io_stress='y'
-list[7]=${io_stress}
-
 #stress test 시간 설정
 time=600
 
@@ -26,13 +8,62 @@ function call_cyclictest(){
 	sudo cyclictest -a -t -n -p99 -D ${time} -h400 -q > output
 }
 
+#가능한 백그라운드 테스트 목록
+list[0]="no_background"
+list[1]="hackbench"
+list[2]="iperf"
+list[3]="cpu_stress"
+list[4]="memory_stress"
+list[5]="hdd_stress"
+list[6]="virtual_memory_stress"
+list[7]="io_stress"
+
+#위 가능한 백그라운드 테스트 인덱스를 이용해 background를 실행할 순서대로 배열에 저장해주세요. 숫자는 띄어쓰기로 구분합니다.
+tests[0]="0" #no_background
+tests[1]="1" #hackbench
+tests[2]="2" #iperf
+tests[3]="3" #cpu_stress
+tests[4]="4" #memory_stress
+tests[5]="5" #hdd_stress
+tests[6]="6" #virtual_memory_stress
+tests[7]="7" #io_stress
+tests[8]="3 5" #cpu_stress and hdd_stress
+
+#백그라운드 명령어 리스트
+commands[0]=""
+commands[1]="hackbench -l 1000000 -s 1024 -P 0"
+commands[2]="iperf -s"
+commands[3]="stress --cpu 16"
+commands[4]="stress --vm 3 --vm-bytes 1024m"
+commands[5]="stress --hdd 3 --hdd-bytes 1024m"
+commands[6]="stress-ng -t 1200 --vm 8 --vm-bytes 80%"
+commands[7]="stress -i 16"
+
+#백그라운드 kill 명령어
+kill_command[0]=""
+kill_command[1]="hackbench"
+kill_command[2]="iperf"
+kill_command[3]="stress"
+kill_command[4]="stress"
+kill_command[5]="stress"
+kill_command[6]="stress"
+kill_command[7]="stress"
+
 echo -n > test.txt
 
 #cyclictest의 결과로부터 max latencies와 max latency를 가져와 출력
 function print(){
   #sort -n : 숫자 정렬, tr : 치환 , Tail -n : n만큼의 라인 출력
   str=`grep "Max Latencies" $1 | tr " " "\n" | tail -16 | sed s/^0*//`
-  echo "**cyclictest result with $2**" >> test.txt
+  echo -n "**cyclictest result with " >> test.txt
+  tmp=(${tests[$2]})
+  echo -n ${list[${tmp[0]}]} >> test.txt
+  for ((j=1; j<${#tmp[*]}; j++))
+  do
+	echo -n ", " >> test.txt
+	echo -n ${list[${tmp[$j]}]} >> test.txt
+  done
+	echo "**" >> test.txt
   echo -n "Max Latencies on each cores : " >> test.txt
   echo ${str} >> test.txt
   echo -n "Max latency : " >> test.txt
@@ -40,63 +71,26 @@ function print(){
   echo >> test.txt
 }
 
-#to kill background process
-function kill_process(){
-  PID=`ps -ef | grep $1 | grep -v grep | awk '{print $2}'`
-  tmp=${#PID}
-  if [ ${tmp} -ne 0 ]; then
-  kill -9 $PID
-  fi
+function fTest(){
+	tmp1=$(echo ${tests[$1]} | tr " " "\n")
+	for a in $tmp1
+	do
+		${commands[${a}]} &
+	done
+	call_cyclictest
 }
 
-#0~list 크기 반복문
-for ((i=0; i<${#list[*]}; i++))
+function fKill(){
+	tmp2=$(echo ${tests[$1]} | tr " " "\n")
+	for k in $tmp2
+	do
+		kill -9 `ps | grep ${kill_command[${k}]} | awk '{print $1}'`
+	done
+}
+
+for ((i=0; i<${#tests[*]}; i++))
 do
-	#no_background
-	if [ ${list[${i}]} = 'y' ] && [ ${i} -eq 0 ]; then
-		call_cyclictest
-		print output no_background
-	#hackbench	
-	elif [ ${list[${i}]} = 'y' ] && [ ${i} -eq 1 ]; then
-		hackbench -l 1000000 -s 1024 -P 0 &
-		call_cyclictest
-		print output hackbench
-		kill_process hackbench
-	#iperf
-	elif [ ${list[${i}]} = 'y' ] && [ ${i} -eq 2 ]; then
-		iperf -s &
-		call_cyclictest
-		print output iperf
-		kill_process iperf
-	#cpu_stress
-	elif [ ${list[${i}]} = 'y' ] && [ ${i} -eq 3 ]; then
-		stress --cpu 16 &
-		call_cyclictest
-		print output cpu_stress
-		kill_process stress
-	#memory_stress
-	elif [ ${list[${i}]} = 'y' ] && [ ${i} -eq 4 ]; then
-		stress --vm 3 --vm-bytes 1024m &
-		call_cyclictest
-		print output memory_stress
-		kill_process stress
-	#hdd_stress
-	elif [ ${list[${i}]} = 'y' ] && [ ${i} -eq 5 ]; then
-		stress --hdd 3 --hdd-bytes 1024m &
-		call_cyclictest
-		print output hdd_stress
-		kill_process stress
-	#virtual_memory_stress
-	elif [ ${list[${i}]} = 'y' ] && [ ${i} -eq 6 ]; then
-		stress-ng -t 1200 --vm 8 --vm-bytes 80% &
-		call_cyclictest
-		print output virtual_memory_stress
-		kill_process stress
-	#io_stress
-	elif [ ${list[${i}]} = 'y' ] && [ ${i} -eq 7 ]; then
-		stress -i 16 &
-  		call_cyclictest
-  		print output io_stress
-  		kill_process stress
-	fi
+		fTest ${i}
+		print output ${i}	
+		fKill ${i}
 done
